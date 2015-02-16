7cef380c4dcfb0279ba2b4b35a3008cd5e2ec1c7
Sarah Sauseng <sarah.sauseng@gmail.com>
2015-02-16 16:33:35 +0100

    Sinnvolle Nachricht

---
+++ Assets/Scripts/testscript/TestPlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayer : MonoBehaviour 
+{
+	public float movementspeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		HandleInput ();
+	}
+
+	void HandleInput() 
+	{
+		if (Input.GetAxis ("Horizontal") > 0) {
+			rigidbody.AddForce(Vector3.right*movementspeed);
+		} else if(Input.GetAxis ("Horizontal") < 0) {
+			this.rigidbody.AddForce(Vector3.left*movementspeed);
+		}
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			this.rigidbody.AddForce(Vector3.forward*movementspeed);
+		} else if(Input.GetAxis ("Vertical") < 0) {
+			this.rigidbody.AddForce(-Vector3.forward*movementspeed);
+		}
+
+		if (Input.GetAxis ("Jump") > 0) {
+			this.rigidbody.AddForce(Vector3.up*50);
+			Debug.Log("PlayerJump");
+		}
+	}
+}

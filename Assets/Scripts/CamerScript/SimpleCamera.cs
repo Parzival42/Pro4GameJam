a71724982b4224ab8393404a56184a56ae5ef626
Parzival Legmaier <legmaier@gmx.at>
2015-02-23 12:58:02 +0100

    Added:  - Simple camera script

---
+++ Assets/Scripts/CamerScript/SimpleCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCamera : MonoBehaviour 
+{
+
+    Transform followObject;
+    public float x_offset = 0;
+    public float z_offset = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+        followObject = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        MoveCamera();
+	}
+
+    private void MoveCamera()
+    {
+        transform.position = new Vector3(followObject.transform.position.x + x_offset, transform.position.y, followObject.transform.position.z + z_offset);
+    }
+}
